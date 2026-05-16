43cc1e812748938ac60923dc2769c80d9ca6b5db
Temsuo <quomhum@gmail.com>
2026-05-16 11:37:38 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import siteConfig from './src/data/site-config';
 
-'https://joaquimedina.github.io'
+// https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://joaquimedina.github.io',
+
     vite: {
         plugins: [tailwindcss()]
     },
+
     integrations: [mdx(), sitemap()]
+});
