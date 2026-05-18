9a43b33ecdcbf192fc724833988890fba4907165
Temsuo <quomhum@gmail.com>
2026-05-18 23:21:58 +0200

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://temsuo.github.io/joaquimedina.github.io/',
+
     base: '/joaquimedina.github.io/',
+
     vite: {
         plugins: [tailwindcss()]
     },
